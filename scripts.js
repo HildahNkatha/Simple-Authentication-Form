4dc9ec1cbd90ddb287f27b7b40428aaeafde9bff
Franklyne-kibet <rootkibet@gmail.com>
2023-05-03 18:19:58 +0300

    fixed form behaviour

--- scripts.js
+++ scripts.js
@@ -2,110 +2,80 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js";
 
-import { getAuth, signInWithEmailAndPassword,createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-auth.js";
-
-
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-auth.js";
 
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-analytics.js";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCpNj9ZvaFMHG_P4G7wTIukl2QrrTFAAXQ",
-  authDomain: "simple-auth-f8b33.firebaseapp.com",
-  projectId: "simple-auth-f8b33",
-  storageBucket: "simple-auth-f8b33.appspot.com",
-  messagingSenderId: "283313934371",
-  appId: "1:283313934371:web:6277309381b2335a0e6196",
-  measurementId: "G-JJK1SSYKYJ"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+// Hide the logout button initially
+document.getElementById('logout-btn').style.display = "none";
 
-
-
-document.getElementById('reg-btn').addEventListener('click', function(){
-
-    document.getElementById('register-div').style.display="inline";
-    document.getElementById('login-div').style.display="none";
-    
-
-
+// Show the registration form on click of the register button
+document.getElementById('reg-btn').addEventListener('click', function () {
+  document.getElementById('register-div').style.display = "inline";
+  document.getElementById('login-div').style.display = "none";
 });
 
-
-document.getElementById('log-btn').addEventListener('click', function(){
- document.getElementById('register-div').style.display="none";
-    document.getElementById('login-div').style.display="inline";
-    
-
-
+// Show the login form on click of the login button
+document.getElementById('log-btn').addEventListener('click', function () {
+  document.getElementById('register-div').style.display = "none";
+  document.getElementById('login-div').style.display = "inline";
 });
 
+// Register a new user on click of the register button
+document.getElementById('register-btn').addEventListener('click', function () {
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
 
-document.getElementById('login-btn').addEventListener('click', function(){
-    const  loginEmail= document.getElementById("login-email").value;
-    const loginPaswword= document.getElementById("login-password").value;
-
-
-    signInWithEmailAndPassword(auth, loginEmail, loginPaswword)
-  .then((userCredential) => {
-
-    const user = userCredential.user;
-    document.getElementById("result-box").style.display="inline";
-    document.getElementById('login-div').style.display="none";
-    document.getElementById("result").innerHTML="Welcome Back ! <br>"+loginEmail +"Login Successfully";
-
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    document.getElementById("result-box").style.display="inline";
-    document.getElementById('login-div').style.display="none";
-    document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
-
-  });
-
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Hide the registration form and show the logout button
+      document.getElementById('register-div').style.display = "none";
+      document.getElementById('logout-btn').style.display = "inline";
+      document.getElementById('result-box').textContent = "Account created successfully.";
+    })
+    .catch((error) => {
+      console.log(error);
+      document.getElementById('result-box').textContent = "Error creating account.";
+    });
 });
 
-  document.getElementById('register-btn').addEventListener('click', function(){
-    const  registerEmail= document.getElementById("register-email").value;
-    const registerPaswword= document.getElementById("register-password").value;
+// Login an existing user on click of the login button
+document.getElementById('login-btn').addEventListener('click', function () {
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
 
-
-    createUserWithEmailAndPassword(auth, registerEmail, registerPaswword)
-  .then((userCredential) => {
-
-    const user = userCredential.user;
-    document.getElementById("result-box").style.display="inline";
-    document.getElementById('register-div').style.display="none";
-    document.getElementById("result").innerHTML="Welcome Back ! <br>"+registerEmail +"Registrater Successfully";
-
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    document.getElementById("result-box").style.display="inline";
-    document.getElementById('register-div').style.display="none";
-    document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
-
-  });
-
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Hide the login form and show the logout button
+      document.getElementById('login-div').style.display = "none";
+      document.getElementById('logout-btn').style.display = "inline";
+      document.getElementById('result-box').textContent = "Login successful.";
+    })
+    .catch((error) => {
+      console.log(error);
+      document.getElementById('result-box').textContent = "Invalid email or password.";
+    });
 });
-document.getElementById('log-out-btn').addEventListener('click', function(){
 
-    signOut(auth).then(() => {
-        document.getElementById("result-box").style.display="none";
-        document.getElementById('login-div').style.display="inline";
-
-      }).catch((error) => {
-        document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
-
-      });
-
-
-
-
+// Logout the user on click of the logout button
+document.getElementById('logout-btn').addEventListener('click', function () {
+  signOut(auth)
+    .then(() => {
+      // Hide the logout button and show the login form
+      document.getElementById('logout-btn').style.display = "none";
+      document.getElementById('login-div').style.display = "inline";
+      // Show the logout message
+      document.getElementById('logout-message').style.display = "block";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
